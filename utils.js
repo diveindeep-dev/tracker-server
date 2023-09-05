98fb32055d7139373ef8d0b21f8b6b0aac1dc1fa
hakyung <diveindeep.dev@gmail.com>
2023-09-05 19:34:21 +0900

    Fix date format

--- utils.js
+++ utils.js
@@ -2,7 +2,8 @@ export const formattedToday = () => {
   const today = new Date();
   const month = today.getMonth() + 1;
   const formattedMonth = month < 10 ? `0${month}` : month;
-  const formatted = `${today.getFullYear()}-${formattedMonth}-${today.getDate()}`;
-
+  const date = today.getDate();
+  const formattedDate = date < 10 ? `0${date}` : date;
+  const formatted = `${today.getFullYear()}-${formattedMonth}-${formattedDate}`;
   return formatted;
 };
